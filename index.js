c74b4e7b59f2cdfb88f84c3cdbbda881768e913c
Nobert-Muma <mumanobert17@gmail.com>
2024-07-04 00:04:07 +0300

    Working with filter method in JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(drivers, driverName) {
+  const name=driverName.toLowerCase();
+  return drivers.filter(drive=>drive.toLowerCase()===name);
+}
+findMatching(drivers, "Bobby");
+findMatching(drivers, 'Sammy');
+findMatching(drivers, 'Susan');
+
+function fuzzyMatch(drivers, drive) {
+  const drr= drive.toLowerCase(); 
+  return drivers.filter(driver => driver.toLowerCase().startsWith(drr));
+}
+fuzzyMatch(drivers, 'Sa');
+fuzzyMatch(drivers, 'y');
+fuzzyMatch(drivers, 'mm');
+
+function matchName(drivers, namee){
+  return drivers.filter(drive=>drive.name===namee)
+}
+matchName(drivers, 'Bobby');
